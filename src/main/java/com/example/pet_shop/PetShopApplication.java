6dd84c21072b6783e2f254f8ff4bf045f8fbaa49
symbat <symbat@example.com>
2026-05-20 11:30:28 +0500

    add async

--- src/main/java/com/example/pet_shop/PetShopApplication.java
+++ src/main/java/com/example/pet_shop/PetShopApplication.java
@@ -2,8 +2,10 @@ package com.example.pet_shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class PetShopApplication {
     public static void main(String[] args) {
         SpringApplication.run(PetShopApplication.class, args);
